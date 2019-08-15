0cd79e3fd2ad542e5be36baa26ba35980b066a12
Victor Tran <vicr12345@gmail.com>
2019-08-15 17:09:47 +1000

    Make sidebar change app state

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,31 @@ import TrendingView from './posts/TrendingView';
 class App extends Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            currentView: "trending"
+        };
+    }
+    
+    currentMainView() {
+        switch (this.state.currentView) {
+            case "trending":
+                return <TrendingView />;
+            default:
+                return <Component />
+        }
     }
     
     render() {
+        let changeView = (view) => {
+            this.setState({
+                currentView: view
+            });
+        };
+        
         return <div className="appContainer">
-            <Sidebar />
-            <TrendingView />
+            <Sidebar currentState={this.state.currentView} onChangeView={changeView} />
+            {this.currentMainView()}
         </div>
     }
 }
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,8 +6,19 @@ class SidebarHeader extends Component {
 }
 
 class SidebarItem extends Component {
+    className() {
+        let classes = [];
+        classes.push("listItem");
+        
+        if (this.props.selected) {
+            classes.push("selected");
+        }
+        
+        return classes.join(" ");
+    }
+    
     render() {
-        return <div className="listItem">
+        return <div className={this.className()} onClick={this.props.onClick}>
             {this.props.text}
         </div>
     }
@@ -15,10 +26,19 @@ class SidebarItem extends Component {
 
 class Sidebar extends Component {
     render() {
+        let isSelected = (stateName) => {
+            if (this.props.currentState === stateName) {
+                console.log("Got state")
+                return true;
+            } else {
+                return false;
+            }
+        };
+        
         return <div className="sidebar">
             <SidebarHeader />
-            <SidebarItem text="Trending" />
-            <SidebarItem text="Leaderboard" />
+            <SidebarItem text="Trending" stateName="trending" selected={isSelected("trending")} onClick={() => this.props.onChangeView("trending")} />
+            <SidebarItem text="Leaderboard" stateName="leaderboard" selected={isSelected("leaderboard")} onClick={() => this.props.onChangeView("leaderboard")} />
         </div>
     }
 }
