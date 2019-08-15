1181a1a41ed07fb4fc94471c21ff5ba06210937d
Michael Noss <michaelmosesnoss@gmail.com>
2019-08-15 17:32:17 +1000

    Update post placeholder text

--- src/posts/Post.jsx
+++ src/posts/Post.jsx
@@ -4,7 +4,7 @@ import Component from '../Component'
 class Post extends Component {
     render() {
         return <div>
-            <p>This is where a post will go.</p>
+            <p>This is where a post will go. Other posts will appear under this post.</p>
         </div>
         
     }
