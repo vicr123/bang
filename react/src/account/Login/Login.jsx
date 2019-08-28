34993b3f45b24372869b6f44c5d141fc798511f5
Victor Tran <vicr12345@gmail.com>
2019-08-28 14:39:31 +1000

    Implement login API calls

--- react/src/account/Login/Login.jsx
+++ react/src/account/Login/Login.jsx
@@ -4,15 +4,53 @@ import Error from '../../Error'
 import "./Login.css";
 
 class Login extends Error {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            currentUsername: "",
+            currentPassword: ""
+        };
+    }
+    
+    logInButtonHandler() {
+        fetch("/api/users/getToken", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.currentUsername,
+                password: this.state.currentPassword
+            })
+        }).then((response) => {
+            if (!response.ok) throw new Error();
+            alert(`Hello ${this.state.currentUsername}`);
+        }).catch(function() {
+            alert("error Error!");
+        })
+    }
+    
     render() {
+        let usernameChange = (e) => {
+            this.setState({
+                currentUsername: e.target.value
+            });
+        }
+        let passwordChange = (e) => {
+            this.setState({
+                currentPassword: e.target.value
+            });
+        }
+        
         return (
-            <div classname="logIn">
+            <div className="logIn">
                 <h1>Log In</h1>
                 <p>Username:</p>
-                <input type="text" username="uname"/>
+                <input type="text" username="uname" value={this.state.currentUsername} onChange={usernameChange}/>
                 <p>Password:</p>
-                <input type="password" password="pword"/>
-                <button classname="button">Log In</button>
+                <input type="password" password="pword" value={this.state.currentPassword} onChange={passwordChange} />
+                <button classname="button" onClick={this.logInButtonHandler.bind(this)}>Log In</button>
             </div>
         );
     }
