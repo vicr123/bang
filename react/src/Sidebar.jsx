f37c5fe64753c01929f8279e538df4ffa76a1ac0
Squizlee <ethanscavia777@gmail.com>
2019-09-17 02:31:57 +1000

    Add emoji/icons to each sidebar item Create CreatePost component and prepare flexbox classes for JSX elements

--- react/src/Sidebar.jsx
+++ react/src/Sidebar.jsx
@@ -4,93 +4,105 @@ import Logo from "./assets/logo.PNG";
 
 // Component that renders the logo
 class SidebarHeader extends Error {
-  render() {
-    return <img className="logo" src={Logo} alt="logo"></img>;
-  }
+	render() {
+		return <img className="logo" src={Logo} alt="logo"></img>;
+	}
 }
 
 // Sidebar elements
 class SidebarItem extends Error {
-  /**
-   * Maintains CSS classes for the Component
-   * @return {String} string containing CSS classes separated by spaces
-   */
-  className() {
-    let classes = [];
-    classes.push("listItem");
+	/**
+	 * Maintains CSS classes for the Component
+	 * @return {String} string containing CSS classes separated by spaces
+	 */
+	className() {
+		let classes = [];
+		classes.push("listItem");
 
-    if (this.props.selected) {
-      classes.push("selected");
-    }
+		if (this.props.selected) {
+			classes.push("selected");
+		}
 
-    return classes.join(" ");
-  }
+		return classes.join(" ");
+	}
 
-  render() {
-    return (
-      <div className={this.className()} onClick={this.props.onClick}>
-        {this.props.text}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className={this.className()} onClick={this.props.onClick}>
+				{this.props.text} {this.props.icon}
+			</div>
+		);
+	}
 }
 
 // Renders sidebar elements
 class Sidebar extends Error {
-    render() {
-        let isSelected = stateName => {
-            if (this.props.currentState === stateName) {
-                console.log("Got state");
-                return true;
-            } else {
-                return false;
-            }
-        }
-    
-        let userItemText;
-        if (this.props.currentLogin.username == null) {
-            userItemText = "Account";
-        } else {
-            userItemText = this.props.currentLogin.username;
-        }
+	render() {
+		let isSelected = stateName => {
+			if (this.props.currentState === stateName) {
+				console.log("Got state");
+				return true;
+			} else {
+				return false;
+			}
+		};
 
-        return (
-            <div className="sidebar scrollable">
-                <SidebarHeader />
-                <SidebarItem
-                    text="Trending"
-                    stateName="trending"
-                    selected={isSelected("trending")}
-                    onClick={() => this.props.onChangeView("trending")}
-                />
-                <SidebarItem 
-                    text="New"
-                    stateName="new"
-                    selected={isSelected("new")}
-                    onClick={() => this.props.onChangeView("new")}
-                />
-                <SidebarItem
-                    text="Leaderboard"
-                    stateName="leaderboard"
-                    selected={isSelected("leaderboard")}
-                    onClick={() => this.props.onChangeView("leaderboard")}
-                />
-                <div style={{ "flex-grow": "1" }} />
-                <SidebarItem
-                    text="About"
-                    stateName="about"
-                    selected={isSelected("about")}
-                    onClick={() => this.props.onChangeView("about")}
-                />
-                <SidebarItem
-                    text={userItemText}
-                    stateName="user"
-                    selected={isSelected("user")}
-                    onClick={() => this.props.onChangeView("user")}
-                />
-            </div>
-        );
-    }
+		let userItemText;
+		if (this.props.currentLogin.username == null) {
+			userItemText = "Account";
+		} else {
+			userItemText = this.props.currentLogin.username;
+		}
+
+		return (
+			<div className="sidebar scrollable">
+				<SidebarHeader />
+				<SidebarItem
+					text="Create a Post"
+					stateName="createPost"
+					selected={isSelected("createPost")}
+					onClick={() => this.props.onChangeView("createPost")}
+					icon="&#10133;"
+				/>
+				<SidebarItem
+					text="Trending"
+					stateName="trending"
+					selected={isSelected("trending")}
+					onClick={() => this.props.onChangeView("trending")}
+					icon="&#127775;"
+				/>
+				<SidebarItem
+					text="New"
+					stateName="new"
+					selected={isSelected("new")}
+					onClick={() => this.props.onChangeView("new")}
+					icon="&#128075;"
+				/>
+				<SidebarItem
+					text="Leaderboard"
+					stateName="leaderboard"
+					selected={isSelected("leaderboard")}
+					onClick={() => this.props.onChangeView("leaderboard")}
+					icon="&#128081;"
+				/>
+				<div style={{ "flex-grow": "1" }} />
+				<SidebarItem
+					text="About"
+					stateName="about"
+					selected={isSelected("about")}
+					onClick={() => this.props.onChangeView("about")}
+					icon="&#129300;"
+				/>
+				<SidebarItem
+					text={userItemText}
+					stateName="user"
+					selected={isSelected("user")}
+					onClick={() => this.props.onChangeView("user")}
+					icon="&#128163;"
+				/>
+			</div>
+		);
+	}
 }
 
 export default Sidebar;
---
+++ react/src/create-post/CreatePost.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Error from "../Error";
+
+class CreatePost extends Error {
+	state = {};
+	render() {
+		return (
+			<React.Fragment>
+				<div className="padded moreSpace postContainer">
+					<h1>$%&*(ing centers now</h1>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default CreatePost;
