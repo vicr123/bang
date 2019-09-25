c475a8c5734b1032d4c5efbeca8b14830c14ac28
Squizlee <ethanscavia777@gmail.com>
2019-09-26 00:08:30 +1000

    Add documentation to backend files New View displays post on click

--- express/api/helpers/resources.js
+++ express/api/helpers/resources.js
@@ -1,46 +1,55 @@
-const express = require('express');
-const fs = require('fs');
-const util = require('util');
-const db = require('../../db/db');
-const settings = require('../../settings');
-const crypto = require('crypto');
-const mimetoext = require('mime-to-extensions');
+const express = require("express");
+const fs = require("fs");
+const util = require("util");
+const db = require("../../db/db");
+const settings = require("../../settings");
+const crypto = require("crypto");
+const mimetoext = require("mime-to-extensions");
 
-const writeFile = util.promisify(fs.writeFile);
-
+const writeFile = util.promisify(fs.writeFile); // makes writing a file return a promise
+/**
+ * returns a random file name with a 16byte hexadecimal number
+ * @param {} extension file name extension to be added to the end of the new file
+ * @returns {} fn filename
+ */
 async function getRandomFileName(extension) {
-    let fn;
-    do {
-        fn = `${crypto.randomBytes(16).toString('hex')}.${extension}`;
-    } while (fs.existsSync(`${settings.get('resourcesPublicDir')}/${fn}`));
-    
-    return fn;
+	let fn;
+	do {
+		fn = `${crypto.randomBytes(16).toString("hex")}.${extension}`;
+	} while (fs.existsSync(`${settings.get("resourcesPublicDir")}/${fn}`));
+
+	return fn;
 }
 
 module.exports = {
-    putResource: async function(data, mimetype) {
-        let extension = mimetoext.extension(mimetype);
-        if (extension == false) throw new Error("Mimetype not resolvable");
-        
-        let randomFileName = await getRandomFileName(extension);
-        await writeFile(`${settings.get('resourcesDir')}/${randomFileName}`, data);
-        
-        await db.insert("Resources", {
-            filename: randomFileName
-        });
-        
-        //Return the file ID and file name
-        return {
-            id: await db.lastInsertId(),
-            filename: randomFileName
-        };
-    },
-    getResource: async function(id) {
-        let rows = await db.select("Resources", ["filename"], "id = ?", [id]);
-        if (rows.length == 0) {
-            return null;
-        } else {
-            return rows[0].filename;
-        }
-    }
-}
+	putResource: async function(data, mimetype) {
+		// get the extension from the mime to extension package
+		let extension = mimetoext.extension(mimetype);
+		if (extension == false) throw new Error("Mimetype not resolvable");
+
+		let randomFileName = await getRandomFileName(extension);
+		// create the file
+		await writeFile(
+			`${settings.get("resourcesDir")}/${randomFileName}`,
+			data
+		);
+		// insert a reference to the resource in the database
+		await db.insert("Resources", {
+			filename: randomFileName
+		});
+
+		//Return the file ID and file name
+		return {
+			id: await db.lastInsertId(),
+			filename: randomFileName
+		};
+	},
+	getResource: async function(id) {
+		let rows = await db.select("Resources", ["filename"], "id = ?", [id]);
+		if (rows.length == 0) {
+			return null;
+		} else {
+			return rows[0].filename;
+		}
+	}
+};
--- express/api/helpers/token.js
+++ express/api/helpers/token.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const db = require('../../db/db');
+const express = require("express");
+const db = require("../../db/db");
 
 module.exports = {
-    getUser: async function(req) {
-        let token = req.get("Authorization");
-        
-        if (!token || !token.startsWith("Token ")) {
-            throw new Error("Invalid Token");
-        }
-        token = token.substr(6);
-        
-        //Retrieve the user ID from the database
-        let rows = await db.select("Tokens", ["userId"], "TOKEN = ?", [token]);
-        if (rows.length == 0) {
-            throw new Error("Invalid Token");
-        }
-        
-        let row = rows[0];
-        
-        //Retrieve the user from the database
-        return await db.select("Users", ["id", "username"], "ID = ?", [row.userId]);
-    }
-}
+	getUser: async function(req) {
+		let token = req.get("Authorization");
+
+		if (!token || !token.startsWith("Token ")) {
+			throw new Error("Invalid Token");
+		}
+		token = token.substr(6); // ignore the Early Token subtring
+
+		//Retrieve the user ID from the database
+		let rows = await db.select("Tokens", ["userId"], "TOKEN = ?", [token]);
+		if (rows.length == 0) {
+			throw new Error("Invalid Token");
+		}
+
+		let row = rows[0];
+
+		//Retrieve the user from the database
+		return await db.select("Users", ["id", "username"], "ID = ?", [
+			row.userId
+		]);
+	}
+};
