e5003f30c38d35f38a7d47c3e557b1bada77b4be
Michael Noss <michaelmosesnoss@gmail.com>
2019-10-03 17:49:20 +1000

    Update main message on About page

--- react/src/about/About.jsx
+++ react/src/about/About.jsx
@@ -4,24 +4,18 @@ import Error from "../Error";
 class About extends Error {
   render() {
     return (
-      <div className="scrollable">
+      <div className="scrollable" style={{flexGrow: 1}} >
         <div className="padded">
           <h1>Image Only Fun</h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci,
-            aliquam inventore. Alias id dolorum explicabo. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Ipsa repellendus deserunt, esse
-            nostrum quasi sunt. Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Consectetur voluptatibus ratione ducimus
-            perferendis. Illo nesciunt voluptas sapiente, eaque praesentium
-            aperiam.
+            Welcome to !BANG, we hope you enjoy your stay.
+            !BANG is an image sharing site only. You will not be able to post any text on this site, just them sweet pics.
           </p>
         </div>
         <div className="HorizontalBox padded">
           <h2>Global Rules</h2>
           <ol>
             <li>All posts are images ONLY</li>
-            <li>Reactions are emojis ONLY</li>
             <li>Attempts to post text are NOT permitted</li>
             <li>Posting of illegal content is NOT permitted</li>
             <li>Impersonating a moderator is strictly prohibited</li>
