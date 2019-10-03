7d03501669a51f54d39712d12705c7d04f9c60bf
Victor Tran <vicr12345@gmail.com>
2019-10-03 18:12:24 +1000

    Finish error handling in account creation screen

--- react/src/account/Login/CreateAccount.jsx
+++ react/src/account/Login/CreateAccount.jsx
@@ -24,9 +24,30 @@ class CreateAccount extends Error {
             localStorage.setItem("loginToken", json.token)
             this.props.onLoginChanged();
         } catch (err) {
-            this.setState({
-                errorState: "That didn't work. Someone else may already have that username."
-            });
+            let showGenericError = true;
+            if (err.status === 400) {
+                let json = await err.json();
+                let state = {
+                    "Missing fields": "Fill in all fields to continue",
+                    "Empty Username": "Username cannot be empty",
+                    "Username greater than 20 characters": "Username must be 20 characters or less",
+                    "Password greater than 128 characters": "Password must be 128 characters or less",
+                    "Password less than 8 characters": "Password must be 8 characters or greater"
+                };
+                
+                if (state.hasOwnProperty(json.error)) {
+                    this.setState({
+                        errorState: state[json.error]
+                    });
+                    showGenericError = false;
+                }
+            }
+            
+            if (showGenericError) {
+                this.setState({
+                    errorState: "That didn't work. Someone else may already have that username."
+                });
+            }
         }
     }
 
@@ -59,12 +80,12 @@ class CreateAccount extends Error {
         return (
             <div className="logIn">
                 <h1>Create Account</h1>
-                <p>
+                <ul>
                     <li>Usernames must be less than 20 characters</li>
                     <li>Usernames with leading or trailing whitespace will be trimmed</li>
                     <li>Passwords must be 8 characters or greater</li>
                     <li>Passwords must be 128 characters or less</li>
-                </p>
+                </ul>
                 <input type="text" username="uname" value={this.state.currentUsername} onChange={usernameChange} onKeyDown={this.onKeyDown.bind(this)} placeholder="Username" />
                 <input type="password" password="pword" value={this.state.currentPassword} onChange={passwordChange} onKeyDown={this.onKeyDown.bind(this)} placeholder="Password" />
                 <button classname="button" onClick={this.registerButtonHandler.bind(this)}>Create Account</button>
