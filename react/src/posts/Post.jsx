2db4dbd60e6425de998296c52ed5a37043ac1ba7
Victor Tran <vicr12345@gmail.com>
2019-10-03 17:49:17 +1000

    Refine error messages for reactions.

--- react/src/posts/Post.jsx
+++ react/src/posts/Post.jsx
@@ -49,7 +49,11 @@ class EmojiButton extends Error {
                 metadata: metadata
             });
         } catch (err) {
-            Modal.mount(<Modal cancelable={true}><h1>Error</h1><div>We couldn't post your reaction. Give it another go.</div></Modal>);
+            Modal.mount(<Modal cancelable={true} title="Couldn't post reaction">
+                <p>We couldn't post your reaction. Give it another go.</p>
+                <p>If you keep hitting problems, try reloading the page.</p>
+                <button onClick={Modal.unmount}>OK</button>
+            </Modal>);
         }
     }
     
@@ -108,7 +112,7 @@ class Post extends Error {
 
     showFlagDialog() {
         if (!Modal.checkLoggedIn()) return;    
-        Modal.mount(<Modal title="Flag" cancelable={true} style={{width: '400px'}}>
+        Modal.mount(<Modal title="Flag" cancelable={true} width={400}>
             <div className="VerticalBox">
                 <span>What's wrong with this post?</span>
                 <button>Contains Text</button>
@@ -131,7 +135,7 @@ class Post extends Error {
             Modal.unmount();
         }
 
-        Modal.mount(<Modal title="Delete" cancelable={true} style={{width: '400px'}}>
+        Modal.mount(<Modal title="Delete" cancelable={true} width={400}>
             <div className="VerticalBox">
                 <span>You are about to delete this post.</span>
                 <span>This cannot be undone.</span>
